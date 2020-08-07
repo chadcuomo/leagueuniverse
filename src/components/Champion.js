3913c3aa99f0f33d05b8d3e494093e563427b7d6
Chad <chad.cuomo@yahoo.com>
2020-08-07 11:15:32 -0400

    fixed champion image links and styling

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -3,13 +3,13 @@ import React from 'react';
 class Champion extends React.Component {
   render() {
     const { name } = this.props.details;
-    const img = `http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${name}.png`;
+    const img = this.props.details.image.full;
+    const imgsrc = `https://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${img}`;
     return (
       <div className="champion-container">
         <div className="champion-inner">
           <h3>{name}</h3>
-          <img src={img} alt={name} />
-          {console.log(this.props.details.image)}
+          <img src={imgsrc} alt={name} className="round-img" />
         </div>
       </div>
     );
